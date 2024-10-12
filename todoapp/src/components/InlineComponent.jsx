b5e9a340cdfb6716580cbe5c2ac4897e9daf35bc
DarwinMelaya <116789003+DarwinMelaya@users.noreply.github.com>
2024-10-12 22:58:37 +0800

    CSS Stylesheet To Style Components

---
+++ todoapp/src/components/InlineComponent.jsx
@@ -0,0 +1,10 @@
+import "../css/style.css";
+const InlineComponent = () => {
+  return (
+    <div>
+      <h1 className="header">Inline Component</h1>
+    </div>
+  );
+};
+
+export default InlineComponent;
